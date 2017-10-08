664b9f4a4ceb802c117277cd3e6f20f8d5c651a3
bgibson10 <bgib1597@gmail.com>
2017-10-08 15:28:04 -0400

    (Added) To Do List
    to do list for this project and possible upcoming

--- Code/CoverLayer.C
+++ Code/CoverLayer.C
@@ -6,3 +6,16 @@
 using namespace std;
 
 ofstream outFile;
+
+
+/*
+    To do list:
+
+    -Clarify design with Hemmick
+    -Add basic skeleton for code
+    -Seperate file for variables?
+    -Privitize the .h files on github
+    -Talk to Klaus about his radial detectors he wants
+
+
+    */
